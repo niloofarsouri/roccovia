110fcfad24dd04b909e9f7af36fc681f1cdce876
niloofar souri <nlfrsouri@gmail.com>
2025-09-23 14:37:59 +0330

    add logo

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,22 @@
 import Image from "next/image";
 
+
+
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+    <div className="items-center justify-items-center min-h-screen sm:p-20">
 
-      <div className="bg-green-950 text-white text-7xl">
+      {/* <div className="bg-green-950 text-[#957536] text-7xl">
         <h1>Roccovia</h1>
+      </div> */}
+
+      <div className="flex justify-center items-center">
+        <Image
+          src="/img/roccovia_logo.jpg"
+          width={300}
+          height={300}
+          alt="Picture of the Logo"
+        />
       </div>
 
       {/* <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
---
+++ src/components/footer/index.js
@@ -0,0 +1,21 @@
+
+
+
+function MyFooter() {
+
+    return (
+        <>
+            <section className="w-full flex justify-center items-center">
+                <div className="w-[80%] flex justify-center items-center text-[#957536] border-t-1 p-2">
+                    <div className="flex-col justify-center items-center">
+                        <p>Niloofar Souri</p>
+                        <p>Minoo Souri</p>
+                    </div>
+                </div>
+            </section>
+        </>
+    )
+}
+
+
+export default MyFooter
