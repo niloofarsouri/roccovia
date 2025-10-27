7aa90ac93c4f52824b2dd58d57487be8060ab090
niloofar souri <nlfrsouri@gmail.com>
2025-10-27 11:38:27 +0330

    Edit in Footer component

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -17,17 +17,17 @@ function MyFooter() {
 
                     <div className="w-[80%] flex justify-between items-center border-t-1 md:p-2">
 
-                        <Link href={'https://www.linkedin.com/in/roccovia-architecture'}>
+                        {/* <Link href={'https://www.linkedin.com/in/roccovia-architecture'}>
                             <Image
                                 src="/img/logo/logo-png.png"
                                 width={200}
                                 height={200}
                                 alt="Picture of the Logo"
                             />
-                        </Link>
+                        </Link> */}
 
-                        <div className="flex-col justify-center items-center">
-                            <Link href={'https://mail.google.com/mail/u/0/#inbox?compose=new'}>
+                        <div className="flex-col justify-center items-center pr-10">
+                            {/* <Link href={'https://mail.google.com/mail/u/0/#inbox?compose=new'}>
                                 <p className="font-serif">Niloofar Souri</p>
                             </Link>
 
@@ -37,8 +37,61 @@ function MyFooter() {
 
                             <Link href={'https://mail.google.com/mail/u/0/#inbox?compose=new'}>
                                 <p className="font-serif">Negin Souri</p>
-                            </Link>
+                            </Link> */}
+
+                            {/* <Link href={'https://mail.google.com/mail/u/0/#inbox?compose=new'}>
+                                <p className="font-serif pl-10">Roccovia Design</p>
+                            </Link> */}
+
+
+
+
+
+                            <ul>
+                                <Link href={'/'}>
+                                    <li className="rounded-xs hover:text-l hover:bg-[#0A2B1B] hover:text-white transition">صفحه اصلی</li>
+                                </Link>
+
+                                {/* <Link href={'/classic'}>
+                                    <li className="rounded-xs hover:text-l bg-[#eee2cb] text-[#2d230f] hover:border-1 transition">Classic</li>
+                                </Link>
+
+                                <Link href={'/neoClassic'}>
+                                    <li className="rounded-xs hover:text-l bg-[#eee2cb] text-[#2d230f] hover:border-1 transition">NeoClassic</li>
+                                </Link>
+
+                                <Link href={'/modern'}>
+                                    <li className="rounded-xs hover:text-l bg-[#eee2cb] text-[#2d230f] hover:border-1 transition">Modern</li>
+                                </Link> */}
+
+                                <Link href={'/landscape'}>
+                                    <li className="rounded-xs hover:text-l hover:bg-[#0A2B1B] hover:text-white transition">Landscape</li>
+                                </Link>
+
+                                <Link href={'/lighting'}>
+                                    <li className="rounded-xs hover:text-l hover:bg-[#0A2B1B] hover:text-white transition">Lighting</li>
+                                </Link>
+
+                                <Link href={'/articles'}>
+                                    <li className="rounded-xs hover:text-l hover:bg-[#0A2B1B] hover:text-white transition">مقالات</li>
+                                </Link>
+
+                                <Link href={'/about'}>
+                                    <li className="rounded-xs hover:text-l hover:bg-[#0A2B1B] hover:text-white transition">درباره ما</li>
+                                </Link>
+
+                            </ul>
                         </div>
+
+
+                        <Link href={'https://www.linkedin.com/in/roccovia-architecture'}>
+                            <Image
+                                src="/img/logo/logo-png.png"
+                                width={200}
+                                height={200}
+                                alt="Picture of the Logo"
+                            />
+                        </Link>
 
                     </div>
                 </section>
